95504e7a9680c3aeb0738dc8382c4ce7e5fd46fd
Jonathan Bangala <jbangala90@gmail.com>
2023-09-30 22:10:01 +0200

    deploy

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -17,22 +17,22 @@ const Item = ({ item, width }) => {
   } = useTheme();
 
   const { category, price, name, image } = item.attributes;
-  const {
-    data: {
-      attributes: {
-        formats: {
-          thumbnail: { url },
-        },
-      },
-    },
-  } = image;
-  // const url =
-  //   image &&
-  //   image.data &&
-  //   image.data.attributes &&
-  //   image.data.attributes.formats &&
-  //   image.data.attributes.formats.medium &&
-  //   image.data.attributes.formats.medium.url;
+  // const {
+  //   data: {
+  //     attributes: {
+  //       formats: {
+  //         thumbnail: { url },
+  //       },
+  //     },
+  //   },
+  // } = image;
+  const url =
+    image &&
+    image.data &&
+    image.data.attributes &&
+    image.data.attributes.formats &&
+    image.data.attributes.formats.medium &&
+    image.data.attributes.formats.medium.url;
   
   return (
     <Box width={width}>
